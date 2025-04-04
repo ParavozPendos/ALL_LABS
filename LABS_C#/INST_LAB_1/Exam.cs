f60aeafec1362f994c9b3af8f0835dd408593ef4
Леонид Саушкин Ноутбук <paravoznedoss@gmail.com>
2025-04-04 10:58:28 +0700

    Разбил программу по классам. Надо добавить class Person и убрать хардкод в Student

---
+++ LABS_C#/INST_LAB_1/Exam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INST_LAB_1
+{
+    class Exam
+    {
+        public Exam()
+        {
+            this.Mark = 0;
+            this.Subject = "None";
+            this.Time = DateTime.MinValue;
+        }
+
+        public Exam(int Mark, string Subject, DateTime Time)
+        {
+            this.Mark = Mark;
+            this.Subject = Subject;
+            this.Time = Time;
+        }
+
+        public int Mark { get; set; }
+        public string Subject { get; set; }
+        public DateTime Time { get; set; }
+
+        public override string ToString()
+        {
+            return $"Предмет: {Subject} | Время: {Time:yyyy-MM-dd} | Оценка: {Mark}";
+        }
+    }
+}
---
+++ LABS_C#/INST_LAB_1/Student.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INST_LAB_1
+{
+    enum Education
+    {
+        Specialist,
+        Bachelor,
+        SecondEducation,
+    }
+
+    class Student
+    {
+        public Student()
+        {
+            Person = "Иванов Иван Иванович";
+            Education = Education.Bachelor;
+            Group = 0;
+            exams = Array.Empty<Exam>();
+        }
+
+        public Student(string Person, Education Education, int Group)
+        {
+            this.Person = Person;
+            this.Education = Education;
+            this.Group = Group;
+            this.exams = Array.Empty<Exam>();
+        }
+
+        public string Person { get; private set; }
+        public Education Education { get; set; }
+        public int Group { get; set; }
+
+        private Exam[] exams;
+        public Exam[] Exams
+        {
+            get { return exams; }
+            set { exams = value ?? Array.Empty<Exam>(); }
+        }
+        private double average;
+        public double Average
+        {
+            get
+            {
+                if (exams == null || exams.Length == 0)
+                    return 0;
+
+                foreach (var item in exams)
+                {
+                    average += item.Mark;
+                }
+                average /= exams.Length;
+
+                return average;
+            }
+        }
+
+        public bool this[Education education]
+        {
+            get => Education == education;
+        }
+
+        public void AddExams(params Exam[] newExams)
+        {
+            Array.Resize(ref exams, exams.Length + newExams.Length);
+            Array.Copy(newExams, 0, exams, exams.Length - newExams.Length, newExams.Length);
+        }
+
+        public override string ToString()
+        {
+            string ExamList = "";
+            foreach (var item in Exams)
+            {
+                ExamList += item.ToString() + "\n";
+            }
+            return $"Студент: {Person} | Образование: {Education} | Группа: {Group}\nЭкзамены:\n{ExamList}";
+        }
+
+        public string ToShortString()
+        {
+            return $"Студент: {Person} | Образование: {Education} | Группа: {Group} | СрБалл: {Average}";
+        }
+    }
+}
