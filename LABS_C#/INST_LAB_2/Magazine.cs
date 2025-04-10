65fd5eb67dd455b81a18a596d0ae06c68fa73108
ParavozPendosPC <Paravoznedoss@gmail.com>
2025-04-11 01:07:25 +0700

    Доделка 2-й лабы

--- LABS_C#/INST_LAB_2/Magazine.cs
+++ LABS_C#/INST_LAB_2/Magazine.cs
@@ -8,27 +8,72 @@ namespace INST_LAB_2
 {
     internal class Magazine
     {
-        private static int MagazineCount;
-        public string Title { get; private set; }
-        public Frequency Period { get; private set; }
-        public DateTime ReleaseDate { get; private set; }
-        
-        private Article[] Articles;
-
+        public string Title { get; set; }
+        public int Count { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Frequency Period { get; set; }        
+        private Article[] _Articles;
+        public Article[] Articles
+        {
+            get { return _Articles; }
+            set { _Articles = value; }
+        }
         public Magazine()
         {
             Title = "NoMAgazineTitle";
             Period = new Frequency();
             ReleaseDate = DateTime.MinValue;
-            MagazineCount += 1;
+            Count = 0;
+            Articles = Array.Empty<Article>();
         }
-        public Magazine(string Title, Frequency Period, DateTime ReleaseDate, int MagazineCount)
+        public Magazine(string Title, Frequency Period, DateTime ReleaseDate, int Count)
         {
             this.Title = Title;
             this.Period = Period;
             this.ReleaseDate = ReleaseDate;
-            MagazineCount += 1;
+            this.Count = Count;
+            Articles = Array.Empty<Article>();
         }
 
+        public double Average
+        {
+            get
+            {
+                if (_Articles == null || _Articles.Length == 0)
+                    return 0;
+
+                double temp = 0;
+                foreach (var article in _Articles)
+                {
+                    temp += article.Raiting;
+                }
+                return temp / _Articles.Length;
+            }
+        }
+        public bool this[Frequency frequency] { get => Period == frequency; }
+
+        public void AddArticles (params Article[] newArticles)
+        {
+            Array.Resize(ref _Articles, _Articles.Length + newArticles.Length);
+            Array.Copy(newArticles, 0, _Articles, _Articles.Length - newArticles.Length, newArticles.Length);
+        }
+
+        public override string ToString()
+        {
+            string ArticleList = "";
+            if (_Articles != null)
+            {
+                foreach (var item in _Articles)
+                {
+                    ArticleList += item.ToString() + "\n\n";
+                }
+            }
+            
+            return $"Название журнала: {Title} | Тираж: {Count} | Время выпуска: {ReleaseDate:yyyy-MM-dd} | Периодичность: {Period} | \n\nСТАТЬИ\n\n{ArticleList}";
+        }
+        public string ToShortString()
+        {
+            return $"Название журнала: {Title} | Тираж: {Count} | Время выпуска: {ReleaseDate:yyyy-MM-dd} | Периодичность: {Period} | \nСредний рейтинг статей: {Average}";
+        }
     }
 }
--- LABS_C#/INST_LAB_2/Program.cs
+++ LABS_C#/INST_LAB_2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +18,35 @@ namespace INST_LAB_2
     {
         static void Main(string[] args)
         {
-            Article article = new Article(new Person("Иван", "Иванов", "Иванович", new DateTime(2004, 8, 7)), "Роботы-пылесосы-убийцы", 100);
-            Console.WriteLine(article.ToString());
+            //PERSONS
+            Person chell_1 = new Person("Гандон", "Дебиляев", "Уродович", new DateTime(2004, 8, 7));
+            Person chell_2 = new Person("Тварь", "Сучеева", "Уродовна", new DateTime(1999, 2, 5));
+            Person chell_3 = new Person("Антон", "Антонов", "Антонович", new DateTime(1888, 4, 2));
+
+            //MAGAZINES
+            Magazine magazine = new Magazine("Произведения искуства", Frequency.Monthly, new DateTime(2000, 5, 3), 743);
+            magazine.AddArticles(
+                new Article(chell_1, "Каловые сталактиты", 100),
+                new Article(chell_2, "Лужи мочи в подъезде", 34),
+                new Article(chell_3, "Обблеванные мусорки", 77)
+            );
+
+            Console.WriteLine("Полная инфа о журнале:");
+            Console.WriteLine(magazine.ToString());
+            Console.WriteLine();
+
+            magazine.AddArticles(
+                new Article(chell_1, "Каловые сталогмиты", 11),
+                new Article(chell_3, "Причины восстания бомжей", 0)
+            );
+
+            Console.WriteLine("Полная инфа о журнале + новые статьи:");
+            Console.WriteLine(magazine.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("короткая инфа о журнале:");
+            Console.WriteLine(magazine.ToShortString());
+            Console.WriteLine();
             Console.ReadKey(true);
         }
     }
