a1ed6715b93411b9c8e087e925bc102fd8617681
ParavozPendosPC <Paravoznedoss@gmail.com>
2025-05-25 23:39:13 +0700

    Создан INST_LAB_8_1

---
+++ LABS_C++/INST_LAB_8_1/INST_LAB_8_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string path = "TEXT.txt";
+    fstream fs;
+
+    fs.open(path, fstream::in | fstream::out | fstream::app);
+
+    if (!fs.is_open()) cout << "ERROR";
+    else
+    {
+       
+    }
+    
+    
+    fs.close();
+    system("pause>nul");
+}
