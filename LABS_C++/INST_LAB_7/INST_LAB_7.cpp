6ba2fc73b79a5db08d4bc0e38f3fda7f341fc5a7
ParavozPendosPC <Paravoznedoss@gmail.com>
2025-05-25 19:09:32 +0700

    Завершил INST_LAB_7

--- LABS_C++/INST_LAB_7/INST_LAB_7.cpp
+++ LABS_C++/INST_LAB_7/INST_LAB_7.cpp
@@ -2,35 +2,33 @@
 #include <string>
 using namespace std;
 
+bool StringChecker(string str)
+{
+    unsigned int left = 0, right = 0, balance = 0;
+
+    while (left < str.size() || right < str.size())
+    {
+        left = str.find('(', left);
+        right = str.find(')', right);
+        if (left != string::npos && left < str.size()) balance++, left++;
+        if (right != string::npos && right < str.size()) balance--, right++;
+        if (balance != 0 || right < left) return 0;
+    }
+    return balance == 0;
+}
+
 int main()
 {
     setlocale(LC_ALL, "");
-    
     string str;
-    unsigned int open[3] = { 0 }, close[3] = {0};
+
 
     cout << "Введите строку" << endl;
-    
     getline(cin, str);
+    if (StringChecker(str)) cout << "Совпадает!";
+    else cout << "Не совпадает!";
 
-    for (int i = 0; i < str.size(); i++)
-    {
-        switch (str[i])
-        {
-            case '{': open[0]++; break;
-            case '[': open[1]++; break;
-            case '(': open[2]++; break;
-            case '}': close[0]++; break;
-            case ']': close[1]++; break;
-            case ')': close[2]++; break;
-            default: break;
-        }
-    }
 
-    if (open[0] == close[0] and open[1] == close[1] and open[2] == close[2])
-        cout << "Кол-во закрывающих и открывающих скобок совпадает";
-    else
-        cout << "Кол-во закрывающих и открывающих скобок отличается";
 
     system("pause>nul");
 }
