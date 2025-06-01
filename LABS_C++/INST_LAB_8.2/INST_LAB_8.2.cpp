d985910a070384866a386751f7605aed771c0b83
ParavozPendosPC <Paravoznedoss@gmail.com>
2025-06-02 05:47:54 +0700

    добавленна лаба 8_2 и 9

---
+++ LABS_C++/INST_LAB_8.2/INST_LAB_8.2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+#include <random>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
+
+    fstream f, g;
+    string fpath = "f.dat";
+    string gpath = "g.dat";
+
+    int count, temp, sum, t1, t2;
+
+    cout << "Введите количество целых числел: ";
+    cin >> count;
+    cout << endl;
+
+    f.open(fpath, ios::in | ios::out |ios::binary | ios::trunc);
+    g.open(gpath, ios::in | ios::out | ios::binary | ios::trunc);
+
+    if (!f.is_open()) cout << "Файл \"f\" не удалось открыть\n";
+    else
+    {
+        cout << "Открытие файла \"f\" успешно!\n\n";
+
+        for (int i = 0; i < count; i++)
+        {
+            temp = rand() % 20;
+            f.write(reinterpret_cast<char*>(&temp), sizeof(int));
+        }
+
+        // для человеческого отображения данных
+        f.seekg(0, ios::beg);
+        for(int i = 0; i < count; i++)
+        {
+            f.read(reinterpret_cast<char*>(&temp), sizeof(int));
+            cout << temp << " ";
+        }
+        cout << "\n\n";
+    }
+
+    if (!g.is_open()) cout << "Файл \"g\" не удалось открыть\n";
+    else
+    {
+        cout << "Открытие файла \"g\" успешно!\n\n";
+        f.seekg(0, ios::beg);
+        g.seekg(0, ios::beg);
+
+        for (int i = 0; i < count-1; i++)
+        {
+            f.seekg(i * sizeof(int), ios::beg);
+            f.read(reinterpret_cast<char*>(&t1), sizeof(int));
+            f.seekg(0, ios::cur);
+            f.read(reinterpret_cast<char*>(&t2), sizeof(int));
+
+            sum = t1 * t2;
+            g.write(reinterpret_cast<char*>(&sum), sizeof(int));
+        }
+
+        // для человеческого отображения данных
+        g.seekg(0, ios::beg);
+        for (int i = 0; i < count - 1; i++)
+        {
+            g.read(reinterpret_cast<char*>(&temp), sizeof(int));
+            cout << temp << " ";
+        }
+        cout << "\n\n";
+    }
+
+    f.close();
+    g.close();
+    system("pause>nul");
+}
---
+++ LABS_C++/INST_LAB_9/INST_LAB_9.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+void main()
+{
+    //Общая часть
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
+
+    
+
+    struct Bagazh
+    {
+        int count;
+        double weight;
+    };
+    Bagazh temp;
+
+    //Часть первой программы
+    fstream writer;
+    int number;
+    
+    cout << "Введите количество багажей для добавления (-1) для отчистки: ";
+    cin >> number;
+    cout << endl;
+    
+    writer.open("Bagazhi.dat", ios::out | ios::app | ios::binary);
+
+    if (!writer.is_open()) cout << "Файл \"writer\" не удалось открыть\n";
+    else
+    {
+        cout << "Открытие файла \"writer\" успешно!\n";
+        if (number == -1)
+        {
+            writer.close();
+            writer.open("Bagazhi.dat", ios::out | ios::binary | ios::trunc); 
+            cout << "Файл очищен!\n";
+        }
+        for (int i = 0; i < number; i++)
+        {
+            temp.count = rand() % 4 + 1;
+            temp.weight = (rand() % 5000) / 100.0;
+            writer.write(reinterpret_cast<char*>(&temp), sizeof(Bagazh));
+        }
+    }
+    writer.close();
+
+    //Часть второй программы
+    fstream reader;
+    bool isFind = false;
+
+    reader.open("Bagazhi.dat", ios::in | ios::ate | ios::binary);
+    if (!reader.is_open()) cout << "Файл \"reader\" не удалось открыть\n";
+    else
+    {
+        cout << "Открытие файла \"reader\" успешно!\n\n";
+
+        reader.seekg(0, ios::beg);
+        while (reader.read(reinterpret_cast<char*>(&temp), sizeof(Bagazh)))
+        {
+            cout << "Число вещей: " << temp.count << "| Вес багажа: " << setw(7) <<right <<temp.weight << " кг" << endl;
+            if (temp.count == 1 && temp.weight <= 30)
+            {
+                isFind = true;
+            }
+        }
+        if (isFind) cout << "\nСреди них был минимум 1 багаж с 1 вещью, весом менее 30кг\n";
+        else cout << "\nСреди них не было ни одного багажа с одной вещью и весом 30кг\n";
+
+        cout << "\n\n";
+    }
+    reader.close();
+
+    system("pause>nul");
+}
