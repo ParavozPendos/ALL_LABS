185a524b2b48496a9bc879042cf13a223ca74648
ParavozPendosPC <Paravoznedoss@gmail.com>
2025-06-02 09:27:33 +0700

    Удалил лишние пробелы

--- LABS_C++/INST_LAB_9/INST_LAB_9.cpp
+++ LABS_C++/INST_LAB_9/INST_LAB_9.cpp
@@ -11,8 +11,6 @@ void main()
     setlocale(LC_ALL, "");
     srand(time(NULL));
 
-    
-
     struct Bagazh
     {
         int count;
@@ -21,59 +19,63 @@ void main()
     Bagazh temp;
 
     //Часть первой программы
-    fstream writer;
-    int number;
-    
-    cout << "Введите количество багажей для добавления (-1) для отчистки: ";
-    cin >> number;
-    cout << endl;
-    
-    writer.open("Bagazhi.dat", ios::out | ios::app | ios::binary);
-
-    if (!writer.is_open()) cout << "Файл \"writer\" не удалось открыть\n";
-    else
     {
-        cout << "Открытие файла \"writer\" успешно!\n";
-        if (number == -1)
-        {
-            writer.close();
-            writer.open("Bagazhi.dat", ios::out | ios::binary | ios::trunc); 
-            cout << "Файл очищен!\n";
-        }
-        for (int i = 0; i < number; i++)
-        {
-            temp.count = rand() % 4 + 1;
-            temp.weight = (rand() % 5000) / 100.0;
-            writer.write(reinterpret_cast<char*>(&temp), sizeof(Bagazh));
-        }
-    }
-    writer.close();
+        fstream writer;
+        int number;
 
-    //Часть второй программы
-    fstream reader;
-    bool isFind = false;
+        cout << "Введите количество багажей для добавления (-1) для отчистки: ";
+        cin >> number;
+        cout << endl;
 
-    reader.open("Bagazhi.dat", ios::in | ios::ate | ios::binary);
-    if (!reader.is_open()) cout << "Файл \"reader\" не удалось открыть\n";
-    else
-    {
-        cout << "Открытие файла \"reader\" успешно!\n\n";
+        writer.open("Bagazhi.dat", ios::out | ios::app | ios::binary);
 
-        reader.seekg(0, ios::beg);
-        while (reader.read(reinterpret_cast<char*>(&temp), sizeof(Bagazh)))
+        if (!writer.is_open()) cout << "Файл \"writer\" не удалось открыть\n";
+        else
         {
-            cout << "Число вещей: " << temp.count << "| Вес багажа: " << setw(7) <<right <<temp.weight << " кг" << endl;
-            if (temp.count == 1 && temp.weight <= 30)
+            cout << "Открытие файла \"writer\" успешно!\n";
+            if (number == -1)
             {
-                isFind = true;
+                writer.close();
+                writer.open("Bagazhi.dat", ios::out | ios::binary | ios::trunc);
+                cout << "Файл очищен!\n";
+            }
+            for (int i = 0; i < number; i++)
+            {
+                temp.count = rand() % 4 + 1;
+                temp.weight = (rand() % 5000) / 100.0;
+                writer.write(reinterpret_cast<char*>(&temp), sizeof(Bagazh));
             }
         }
-        if (isFind) cout << "\nСреди них был минимум 1 багаж с 1 вещью, весом менее 30кг\n";
-        else cout << "\nСреди них не было ни одного багажа с одной вещью и весом 30кг\n";
-
-        cout << "\n\n";
+        writer.close();
     }
-    reader.close();
+
+    //Часть второй программы
+    {
+        fstream reader;
+        bool isFind = false;
+
+        reader.open("Bagazhi.dat", ios::in | ios::ate | ios::binary);
+        if (!reader.is_open()) cout << "Файл \"reader\" не удалось открыть\n";
+        else
+        {
+            cout << "Открытие файла \"reader\" успешно!\n\n";
+
+            reader.seekg(0, ios::beg);
+            while (reader.read(reinterpret_cast<char*>(&temp), sizeof(Bagazh)))
+            {
+                cout << "Число вещей: " << temp.count << "| Вес багажа: " << setw(7) << right << temp.weight << " кг" << endl;
+                if (temp.count == 1 && temp.weight <= 30)
+                {
+                    isFind = true;
+                }
+            }
+            if (isFind) cout << "\nСреди них был минимум 1 багаж с 1 вещью, весом менее 30кг\n";
+            else cout << "\nСреди них не было ни одного багажа с одной вещью и весом 30кг\n";
+
+            cout << "\n\n";
+        }
+        reader.close();
+    }
 
     system("pause>nul");
 }
